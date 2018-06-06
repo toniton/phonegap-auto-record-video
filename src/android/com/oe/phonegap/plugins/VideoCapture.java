023e20063aeb0bf2845fdc3025458e860147d5a6
Toniton <akinjiolatoni@gmail.com>
2018-06-06 01:59:13 +0100

    Tried resource getApplication

--- src/android/com/oe/phonegap/plugins/VideoCapture.java
+++ src/android/com/oe/phonegap/plugins/VideoCapture.java
@@ -23,7 +23,7 @@ import java.io.IOException;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.content.res.Resources; 
+import android.content.res.Resources;
 import android.hardware.Camera;
 import android.media.CamcorderProfile;
 import android.media.MediaRecorder;
@@ -39,14 +39,14 @@ import android.widget.TextView;
 
 public class VideoCapture extends Activity implements SurfaceHolder.Callback {
 
-    public static final String INTENT_DURATION = "android.intent.extra.durationLimit";
+	public static final String INTENT_DURATION = "android.intent.extra.durationLimit";
 
 	private MediaRecorder recorder;
 	private SurfaceHolder holder;
 	private CamcorderProfile camcorderProfile;
 	private Camera camera;
 	private File outFile;
-	
+
 	boolean recording = false;
 	boolean usecamera = true;
 	boolean previewRunning = false;
@@ -54,52 +54,51 @@ public class VideoCapture extends Activity implements SurfaceHolder.Callback {
 
 	Handler timer = new Handler();
 
-	
 	int current = 0;
 	int duration = 10;
-	
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-				
+
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
-		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-				WindowManager.LayoutParams.FLAG_FULLSCREEN);
+		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
 		camcorderProfile = CamcorderProfile.get(CamcorderProfile.QUALITY_LOW);
 
-		Resources resources = getApplication().getResources(); 
+		Resources resources = resources;
 		// ;
 		// getActivity().findViewById(
 		// getActivity().getResources().getIdentifier(
 		// getActivity().getPackageName()
 		// setContentView(R.layout.auto_record_video_layout);
-		setContentView(
-			resources.getIdentifier("auto_record_video_layout", "layout", cordova.getActivity().getPackageName())
-		);
+		setContentView(resources.getIdentifier("auto_record_video_layout", "layout", getActivity().getPackageName()));
 
-		SurfaceView cameraView = (SurfaceView) findViewById(cordova.getResource("CameraView", "id"));
+		SurfaceView cameraView = (SurfaceView) findViewById(
+				resources.getIdentifier("CameraView", "id", getApplication().getPackageName()));
 		holder = cameraView.getHolder();
 		holder.addCallback(this);
 		holder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
 
 		duration = getIntent().getIntExtra(INTENT_DURATION, duration);
-		
-		currentTime = (TextView) findViewById(cordova.getResource("currenttime", "id"));
-		durationTime = (TextView) findViewById(cordova.getResource("duration", "id"));
-		
-		currentTime.setText(String.valueOf(current));
-		durationTime.setText(String.valueOf(duration)+" seconds");
 
-//		cameraView.setClickable(true);
-//		cameraView.setOnClickListener(this);
-		
+		currentTime = (TextView) findViewById(
+				resources.getIdentifier("currenttime", "id", getApplication().getPackageName()));
+		durationTime = (TextView) findViewById(
+				resources.getIdentifier("duration", "id", getApplication().getPackageName()));
+
+		currentTime.setText(String.valueOf(current));
+		durationTime.setText(String.valueOf(duration) + " seconds");
+
+		// cameraView.setClickable(true);
+		// cameraView.setOnClickListener(this);
+
 		// Sleep 1s
 		Runnable r = new Runnable() {
-		    @Override
-		    public void run(){
-		    	initAutoRecord();
-		    }
+			@Override
+			public void run() {
+				initAutoRecord();
+			}
 		};
 
 		Handler h = new Handler();
@@ -108,25 +107,26 @@ public class VideoCapture extends Activity implements SurfaceHolder.Callback {
 
 	private void prepareRecorder() {
 		OELog.d("Prepare recorder here");
-        recorder = new MediaRecorder();
+		recorder = new MediaRecorder();
 		recorder.setPreviewDisplay(holder.getSurface());
-		
+
 		if (usecamera) {
 			camera.unlock();
 			recorder.setCamera(camera);
 		}
-		
+
 		recorder.setAudioSource(MediaRecorder.AudioSource.DEFAULT);
 		recorder.setVideoSource(MediaRecorder.VideoSource.DEFAULT);
 
 		recorder.setProfile(camcorderProfile);
-		
-		OELog.d("file format:"+camcorderProfile.fileFormat);
+
+		OELog.d("file format:" + camcorderProfile.fileFormat);
 
 		// This is all very sloppy
 		if (camcorderProfile.fileFormat == MediaRecorder.OutputFormat.THREE_GPP) {
-        	try {
-				File newFile = File.createTempFile("AutoRecordVideo", ".3gp", Environment.getExternalStorageDirectory());
+			try {
+				File newFile = File.createTempFile("AutoRecordVideo", ".3gp",
+						Environment.getExternalStorageDirectory());
 				recorder.setOutputFile(newFile.getAbsolutePath());
 				outFile = newFile;
 			} catch (IOException e) {
@@ -135,8 +135,9 @@ public class VideoCapture extends Activity implements SurfaceHolder.Callback {
 				finish();
 			}
 		} else if (camcorderProfile.fileFormat == MediaRecorder.OutputFormat.MPEG_4) {
-        	try {
-				File newFile = File.createTempFile("AutoRecordVideo", ".mp4", Environment.getExternalStorageDirectory());
+			try {
+				File newFile = File.createTempFile("AutoRecordVideo", ".mp4",
+						Environment.getExternalStorageDirectory());
 				recorder.setOutputFile(newFile.getAbsolutePath());
 				outFile = newFile;
 			} catch (IOException e) {
@@ -145,8 +146,9 @@ public class VideoCapture extends Activity implements SurfaceHolder.Callback {
 				finish();
 			}
 		} else {
-        	try {
-				File newFile = File.createTempFile("AutoRecordVideo", ".mp4", Environment.getExternalStorageDirectory());
+			try {
+				File newFile = File.createTempFile("AutoRecordVideo", ".mp4",
+						Environment.getExternalStorageDirectory());
 				recorder.setOutputFile(newFile.getAbsolutePath());
 				outFile = newFile;
 			} catch (IOException e) {
@@ -155,11 +157,11 @@ public class VideoCapture extends Activity implements SurfaceHolder.Callback {
 				finish();
 			}
 		}
-		
-		OELog.d("duration:"+duration*1000);
-		recorder.setMaxDuration(duration*1000);
-//		recorder.setMaxFileSize(5000000); // Approximately 5 megabytes
-		
+
+		OELog.d("duration:" + duration * 1000);
+		recorder.setMaxDuration(duration * 1000);
+		// recorder.setMaxFileSize(5000000); // Approximately 5 megabytes
+
 		try {
 			recorder.prepare();
 		} catch (IllegalStateException e) {
@@ -172,19 +174,19 @@ public class VideoCapture extends Activity implements SurfaceHolder.Callback {
 	}
 
 	final Runnable countDown = new Runnable() {
-	    @Override
-	    public void run(){
-	    	current++;
-	    	if(current>=duration) {
-	    		timer.removeCallbacks(countDown);
-	    		stopAutoRecord();
-	    	} else {
-	    		OELog.d("Current time : "+current);
-	    		currentTime.setText(String.valueOf(current));
-	    		
-	    		timer.postDelayed(countDown, 1000);
-	    	}
-	    }
+		@Override
+		public void run() {
+			current++;
+			if (current >= duration) {
+				timer.removeCallbacks(countDown);
+				stopAutoRecord();
+			} else {
+				OELog.d("Current time : " + current);
+				currentTime.setText(String.valueOf(current));
+
+				timer.postDelayed(countDown, 1000);
+			}
+		}
 	};
 
 	public void initAutoRecord() {
@@ -194,7 +196,7 @@ public class VideoCapture extends Activity implements SurfaceHolder.Callback {
 
 		countDown.run();
 	}
-	
+
 	public void stopAutoRecord() {
 		if (recording) {
 			recorder.stop();
@@ -208,11 +210,11 @@ public class VideoCapture extends Activity implements SurfaceHolder.Callback {
 			recorder.release();
 			recording = false;
 			OELog.d("Recording Stopped");
-			
+
 			// finish activity
 			finish();
-			
-//			prepareRecorder();
+
+			// prepareRecorder();
 		} else {
 			OELog.d("You cannot stop record because not recording now.");
 		}
@@ -220,54 +222,51 @@ public class VideoCapture extends Activity implements SurfaceHolder.Callback {
 
 	public void surfaceCreated(SurfaceHolder holder) {
 		OELog.d("surfaceCreated");
-		
+
 		if (usecamera) {
 			camera = Camera.open();
-			
+
 			try {
 				camera.setDisplayOrientation(90);
 				camera.setPreviewDisplay(holder);
 				camera.startPreview();
 				previewRunning = true;
-			}
-			catch (IOException e) {
+			} catch (IOException e) {
 				OELog.e(e.getMessage());
 				e.printStackTrace();
-			}	
-		}		
-		
+			}
+		}
+
 	}
 
 	public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
 		OELog.d("surfaceChanged");
 
 		if (!recording && usecamera) {
-			if (previewRunning){
+			if (previewRunning) {
 				camera.stopPreview();
 			}
 
 			try {
 				Camera.Parameters p = camera.getParameters();
 
-				 p.setPreviewSize(camcorderProfile.videoFrameWidth, camcorderProfile.videoFrameHeight);
-			     p.setPreviewFrameRate(camcorderProfile.videoFrameRate);
-				
+				p.setPreviewSize(camcorderProfile.videoFrameWidth, camcorderProfile.videoFrameHeight);
+				p.setPreviewFrameRate(camcorderProfile.videoFrameRate);
+
 				camera.setParameters(p);
-				
+
 				camera.setDisplayOrientation(90);
 				camera.setPreviewDisplay(holder);
 				camera.startPreview();
 				previewRunning = true;
-			}
-			catch (IOException e) {
+			} catch (IOException e) {
 				OELog.e(e.getMessage());
 				e.printStackTrace();
-			}	
-			
-			prepareRecorder();	
+			}
+
+			prepareRecorder();
 		}
 	}
-
 
 	public void surfaceDestroyed(SurfaceHolder holder) {
 		OELog.d("surfaceDestroyed");
@@ -278,21 +277,21 @@ public class VideoCapture extends Activity implements SurfaceHolder.Callback {
 		recorder.release();
 		if (usecamera) {
 			previewRunning = false;
-			//camera.lock();
+			// camera.lock();
 			camera.release();
 		}
-		
+
 	}
-	
+
 	@Override
 	public void finish() {
 		// TODO Auto-generated method stub
-		
+
 		Intent data = new Intent();
-		OELog.d("File: "+Uri.fromFile(outFile));
+		OELog.d("File: " + Uri.fromFile(outFile));
 		data.setData(Uri.fromFile(outFile));
 		setResult(Activity.RESULT_OK, data);
-		
+
 		super.finish();
 	}
 }
